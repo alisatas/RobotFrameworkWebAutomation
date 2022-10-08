9792878e0a5adeebf85c890536464d1fefc9bb45
Ali isa Tas <alisatas9292@gmail.com>
2022-10-08 22:03:59 +0200

    firefox driver added

--- base/driver.py
+++ base/driver.py
@@ -1,6 +1,5 @@
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
-
 def get_chrome_driver():
     chrome_driver = ChromeDriverManager().install()
     return chrome_driver
